05d1a31a31076422d8979f9c5d1cb03b7e532092
appcreator24kenya <101715421+appcreator24kenya@users.noreply.github.com>
2022-11-28 17:51:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("summary").click(function(){
+      $("fa-angle-down , one").toggleClass("fa-angle-up , one");
+    });
+});
+$(document).ready(function(){
+    $("summary").click(function(){
+      $("fa-angle-down , two").toggleClass("fa-angle-up , two");
+    });
+});
+$(document).ready(function(){
+    $("summary").click(function(){
+      $("fa-angle-down , three").toggleClass("fa-angle-up , three");
+    });
+});
+$(document).ready(function(){
+    $("summary").click(function(){
+      $("fa-angle-down , four").toggleClass("fa-angle-up , four");
+    });
+});
+$(document).ready(function(){
+    $("summary").click(function(){
+      $("fa-angle-down , five").toggleClass("fa-angle-up , five");
+    });
+});
